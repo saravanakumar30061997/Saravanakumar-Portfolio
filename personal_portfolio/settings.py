2717f606f0d7388abf007e926f334ea28090a686
Saravanakumar S <saravanask1997@gmail.com>
2025-02-21 20:35:37 +0530

    2 commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddeware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'personal_portfolio.urls'
