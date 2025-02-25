ca8cccc7ca6527213bc2dc7366092f68ae7b1ddc
Saravanakumar S <saravanask1997@gmail.com>
2025-02-25 18:49:39 +0530

    few changes

--- portfolio/models.py
+++ portfolio/models.py
@@ -11,7 +11,7 @@ class PortfolioItem(models.Model):
     item_type = models.CharField(max_length=20, choices=ITEM_TYPES, null=True)
     skill = models.ForeignKey(Skills,on_delete=models.CASCADE,related_name='certifications',null=True)
     description = models.TextField(null=True, blank=True)
-    image = models.ImageField('Portfolio', null= True)
+    image = CloudinaryField('Portfolio', null=True)
     url = models.URLField(blank=True)
 
     def __str__(self):
